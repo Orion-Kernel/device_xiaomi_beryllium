edbcbf936813273b3465f6613378bd48393b3c2a
Orion Kernel <79272711+Orion-Kernel@users.noreply.github.com>
2022-06-25 21:23:28 +0530

    Update lineage_beryllium.mk

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -25,7 +25,7 @@ PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
     TARGET_PRODUCT="beryllium"
 
 BUILD_FINGERPRINT := Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys
